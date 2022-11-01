b7f703c1072df6a9492620e7359a991cb1333f67
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2022-11-01 22:19:55 +0100

    workink validation

--- index.js
+++ index.js
@@ -10,11 +10,82 @@ let incomeTable = [];
 
 // Class for Income
 class IncomeList {
-  constructor(incomeID, incomeNameInput, incomeValueInput, incomeEditID) {
+  constructor(
+    incomeID,
+    incomeNameInputClass,
+    incomeValueInputClass,
+    incomeEditID
+  ) {
     this.incomeID = "Income" + incomeID;
-    this.incomeValueInput = incomeValueInput;
-    this.incomeNameInput = incomeNameInput;
+    this.incomeValueInputClass = incomeValueInputClass;
+    this.incomeNameInputClass = incomeNameInputClass;
     this.incomeEditID = "IncomeEdit" + incomeEditID;
+  }
+}
+//validation
+function incomeCheck() {
+  const incomeName = incomeNameInput.value;
+  let incomeValue = parseInt(incomeValueInput.value);
+  if (incomeValue < 0) {
+    incomeValueInput.setAttribute("placeholder", "Za niska wartość");
+    incomeValueInput.style.backgroundColor = "red";
+    incomeValueInput.value = "";
+    let time = setTimeout(() => {
+      incomeValueInput.setAttribute("placeholder", "Wpisz wartość");
+      incomeValueInput.style.backgroundColor = "white";
+    }, 5000);
+    return time;
+  } else if (incomeValue === 0) {
+    incomeValueInput.setAttribute("placeholder", "Zła wartość");
+    incomeValueInput.style.backgroundColor = "red";
+    incomeValueInput.value = "";
+    let timeZero = setTimeout(() => {
+      incomeValueInput.setAttribute("placeholder", "Wpisz wartość");
+      incomeValueInput.style.backgroundColor = "white";
+    }, 5000);
+    return timeZero;
+  } else if (incomeValueInput.value === "") {
+    incomeValueInput.setAttribute("placeholder", "Zła wartość");
+    incomeValueInput.style.backgroundColor = "red";
+    incomeValueInput.value = "";
+    let timeNaN = setTimeout(() => {
+      incomeValueInput.setAttribute("placeholder", "Wpisz wartość");
+      incomeValueInput.style.backgroundColor = "white";
+    }, 5000);
+    return timeNaN;
+  } else {
+    const newincome = new IncomeList(
+      incomeID,
+      incomeNameInput.value,
+      incomeValueInput.value,
+      incomeEditID
+    );
+    incomeTable.push(newincome);
+    incomeID++;
+    incomeEditID++;
+    const listIncome = document.createElement("li");
+    const divIncomeName = document.createElement("div");
+    const divIncomeValue = document.createElement("div");
+    const buttonIncomeEdit = document.createElement("button");
+    const buttonIncomeRemove = document.createElement("button");
+    buttonIncomeRemove.innerHTML = "Usuń";
+    buttonIncomeEdit.innerHTML = "Edytuj";
+    buttonIncomeRemove.setAttribute("class", "incomeRemove");
+    listIncome.setAttribute("display", "flex");
+    divIncomeName.setAttribute("class", "listDiv");
+    divIncomeValue.setAttribute("class", "listDiv");
+    incomeUl.appendChild(listIncome);
+    listIncome.appendChild(divIncomeName);
+    listIncome.appendChild(divIncomeValue);
+    listIncome.appendChild(buttonIncomeEdit);
+    listIncome.appendChild(buttonIncomeRemove);
+
+    incomeTable.forEach((element) => {
+      listIncome.id = element.incomeID;
+      buttonIncomeEdit.id = element.incomeEditID;
+      divIncomeName.innerHTML = element.incomeNameInputClass;
+      divIncomeValue.innerHTML = element.incomeValueInputClass;
+    });
   }
 }
 
@@ -34,7 +105,7 @@ function appendList() {
 function sumInput() {
   const incomeDiv = document.querySelector(".sumAll");
   const arrayValue = incomeTable.map((number) =>
-    parseInt(number.incomeValueInput)
+    parseInt(number.incomeValueInputClass)
   );
   const sum = arrayValue.reduce((acc, number) => {
     return acc + number;
@@ -63,8 +134,8 @@ function incomeForEach() {
   incomeTable.forEach((element) => {
     listIncome.id = element.incomeID;
     buttonIncomeEdit.id = element.incomeEditID;
-    divIncomeName.innerHTML = element.incomeNameInput;
-    divIncomeValue.innerHTML = element.incomeValueInput;
+    divIncomeName.innerHTML = element.incomeNameInputClass;
+    divIncomeValue.innerHTML = element.incomeValueInputClass;
   });
 }
 
@@ -77,10 +148,8 @@ function incomeForEach() {
 // Create income list
 
 //active all functions
-// incomeAddButton.addEventListener("click", addList);
-incomeAddButton.addEventListener("click", appendList);
-incomeAddButton.addEventListener("click", sumInput);
-incomeAddButton.addEventListener("click", incomeForEach);
 
-let tab = incomeTable;
-console.log(tab);
+// incomeAddButton.addEventListener("click", appendList);
+incomeAddButton.addEventListener("click", sumInput);
+// incomeAddButton.addEventListener("click", incomeForEach);
+incomeAddButton.addEventListener("click", incomeCheck);
