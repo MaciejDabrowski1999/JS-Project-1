c40b774f69e7486fd10d1c1be38c87aa77ec81cd
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2022-11-06 20:28:14 +0100

    add first code eddit button

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function incomeCheck() {
       listIncome.setAttribute("class", "incomeList");
       divIncomeName.setAttribute("class", "listDiv");
       divIncomeValue.setAttribute("class", "listDiv");
-      buttonIncomeEdit.setAttribute("onClick", "editTask(this.id)");
+      buttonIncomeEdit.setAttribute("onClick", "editIncome(this.id)");
       incomeUl.appendChild(listIncome);
       listIncome.appendChild(divIncomeName);
       listIncome.appendChild(divIncomeValue);
@@ -106,11 +106,11 @@ function incomeCheck() {
       listIncome.appendChild(buttonIncomeRemove);
 
       incomeTable.forEach((element) => {
-        listIncome.id = element.incomeID;
-        buttonIncomeEdit.id = element.incomeID;
-        buttonIncomeRemove.id = element.incomeID;
-        divIncomeName.id = element.incomeID;
-        divIncomeValue.id = element.incomeID;
+        listIncome.id = "incomeList" + element.incomeID;
+        buttonIncomeEdit.id = "incomeEdit" + element.incomeID;
+        buttonIncomeRemove.id = "incomeDelete" + element.incomeID;
+        // divIncomeName.id = "incomeDivName" + element.incomeID;
+        // divIncomeValue.id = "incomeDivValue" + element.incomeID;
         divIncomeName.innerHTML = element.incomeNameInputClass;
         divIncomeValue.innerHTML = element.incomeValueInputClass;
       });
@@ -134,31 +134,114 @@ function incomeCheck() {
 
 //editfunction
 
-function editTask(editIncome) {
+function editIncome(editIncome) {
   let editIdArray = [];
   editIdArray.push(editIncome);
   const editNumber = editIdArray[0].split("");
-  let idNumber = editNumber[10];
-  console.log(idNumber);
-  let numId = parseInt(idNumber);
-}
+  console.log(editNumber);
+  //find id
+  function findID() {
+    console.log(editNumber);
+    let idNumber = editNumber[10];
+    let numId = parseInt(idNumber);
 
+    incomeNameInput.value = incomeTable[numId].incomeNameInputClass;
+    incomeValueInput.value = incomeTable[numId].incomeValueInputClass;
+  }
+  function updateIncome() {
+    let idNumber = editNumber[10];
+    let numId = parseInt(idNumber);
+    console.log(numId);
+    const incomeName = incomeNameInput.value;
+    const incomeValue = incomeValueInput.value;
+    incomeTable[numId].incomeNameInputClass.push(incomeName);
+    incomeTable[numId].incomeValueInputClass.push(incomeValue);
+  }
+  function createUpdateButton() {
+    const updateButton = document.createElement("button");
+    updateButton.innerHTML = "Zapisz";
+    updateButton.id = "incomeUpdateButton";
+    updateButton.setAttribute("class", "updateButton");
+    updateButton.setAttribute("onClick", "updateIncome()");
+    const incomeDiv = document.querySelector(".mainConsole");
+    incomeDiv.appendChild(updateButton);
+  }
+  // incomeAddButton.remove();
+  createUpdateButton();
+  findID();
+  updateIncome();
+}
+function updateIncome() {
+  const incomeName = incomeNameInput.value;
+  const incomeValue = incomeValueInput.value;
+  incomeTable[numId].incomeNameInputClass.push(incomeName);
+  incomeTable[numId].incomeValueInputClass.push(incomeValue);
+}
 //Remove function
 function deleteIncome(deleteIncome) {
-  let idNumber = deleteIncome;
-  // let deleteIdArray = [];
-  // deleteIdArray.push(deleteIncome);
-  // const deleteNumber = deleteIdArray[0].split("");
-  // let deleteNumberCount = deleteNumber.length;
-
-  let numId = parseInt(idNumber);
-  for (let i = 0; i < incomeTable.length; i++)
-    if (incomeTable[i].incomeID === numId) {
-      incomeTable.splice(i, 1);
-      break;
-    }
-  let element = document.querySelector("#income\\" + "#\\" + idNumber);
-  element.remove();
+  //id button and list
+  let deleteIdArray = [];
+  deleteIdArray.push(deleteIncome);
+  const deleteNumber = deleteIdArray[0].split("");
+  // validation for above 100 income
+  if (incomeTable.length < 10 && deleteNumber.length === 13) {
+    let idNumber = deleteNumber[12];
+    let numId = parseInt(idNumber);
+    for (let i = 0; i < incomeTable.length; i++)
+      if (incomeTable[i].incomeID === numId) {
+        incomeTable.splice(i, 1);
+        break;
+      }
+    let element = document.querySelector("#incomeList" + idNumber);
+    element.remove();
+  } else if (incomeTable.length > 10 && deleteNumber.length === 13) {
+    let idNumber = deleteNumber[12];
+    let numId = parseInt(idNumber);
+    for (let i = 0; i < incomeTable.length; i++)
+      if (incomeTable[i].incomeID === numId) {
+        incomeTable.splice(i, 1);
+        break;
+      }
+    let element = document.querySelector("#incomeList" + idNumber);
+    element.remove();
+  } else if (incomeTable.length > 10 && deleteNumber.length === 14) {
+    let num1 = deleteNumber[12];
+    let num2 = deleteNumber[13];
+    let idNumber = num1 + num2;
+    let numId = parseInt(idNumber);
+    for (let i = 0; i < incomeTable.length; i++)
+      if (incomeTable[i].incomeID === numId) {
+        incomeTable.splice(i, 1);
+        break;
+      }
+    let element = document.querySelector("#incomeList" + idNumber);
+    element.remove();
+  } else if ((incomeTable.length = 10 && deleteNumber.length === 14)) {
+    let num1 = deleteNumber[12];
+    let num2 = deleteNumber[13];
+    let idNumber = num1 + num2;
+    let numId = parseInt(idNumber);
+    for (let i = 0; i < incomeTable.length; i++)
+      if (incomeTable[i].incomeID === numId) {
+        incomeTable.splice(i, 1);
+        break;
+      }
+    let element = document.querySelector("#incomeList" + idNumber);
+    element.remove();
+  } else {
+    let num1 = deleteNumber[12];
+    let num2 = deleteNumber[13];
+    let num3 = deleteNumber[14];
+    let idNumber = num1 + num2 + num3;
+    let numId = parseInt(idNumber);
+    for (let i = 0; i < incomeTable.length; i++)
+      if (incomeTable[i].incomeID === numId) {
+        incomeTable.splice(i, 1);
+        break;
+      }
+    let element = document.querySelector("#incomeList" + idNumber);
+    element.remove();
+  }
 
   //update sum div
   const sumAll = () => {
